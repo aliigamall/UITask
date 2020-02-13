86a0697efb730519a688777a39de9e8931c549ad
maher ahmed <maher.ahmed355@yahoo.com>
2020-02-13 21:58:41 +0200

    Fix alignment navbar, Fix width slider images, Add DinNext font family, Add tab to news section, Fix indictors position in survey slider, change darker shadow in survey box, Fix alignment in copyright section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,22 @@
 $(document).ready(function(){
+
+    // Slider Height
+
     const winH = $(window).height();
 
     $("header, header .carousel__carousel-inner--item").height(winH);
+
+    // News Tabs
+
+    $("#change-tab li").click(function(){
+        $(this).siblings().removeClass("active");
+        $(this).addClass("active");
+
+        $('#border-active .border-space__seprated').toggleClass("active")
+    });
+
+
+    // ScrollTop Button
 
     $(window).scroll(function(){
 
@@ -10,7 +25,7 @@ $(document).ready(function(){
         else
             $(".scroll-top").hide()
             
-    })
+    });
 
     $('.scroll-top').click(function () {
         $("html, body").animate({
